ab3f30144e503cc6064bc87a077e99109afcff98
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2023-10-30 13:14:12 -0700

    initial user story endpoints implemented

--- backend/open-recipes/main.py
+++ backend/open-recipes/main.py
@@ -8,7 +8,7 @@ from typing import List, Union
 
 from fastapi import FastAPI
 
-from models import Ingredient, Recipe, RecipeList, Review, User, PopulatedRecipe
+from models import Ingredient, Recipe, RecipeList, Review, User, PopulatedRecipe, CreateUserRequest, CreateRecipeListRequest, CreateRecipeRequest
 from database import engine 
 from sqlalchemy import text
 import uvicorn
@@ -52,38 +52,51 @@ def post_ingredients(body: Ingredient) -> Union[None, Ingredient]:
     """
     pass
 
-
+#SMOKE TESTED
 @app.get('/recipe-lists', response_model=List[RecipeList])
 def get_recipe_lists() -> List[RecipeList]:
     """
     Get all recipe lists
     """
+    recipeListAll = []
+    with engine.begin() as conn:
+        result = conn.execute(text(f"SELECT id, name, description FROM recipe_list ORDER BY id"))
+        rows = result.fetchall()
+        for row in rows: 
+            id, name, description = row
+            recipe = RecipeList(id=id, name=name, description=description)
+            recipeListAll.append(recipe)
+        return recipeListAll
     pass
 
-@app.get('/recipe-lists/', response_model=List[RecipeList])
-def get_recipe_lists() -> List[RecipeList]:
-    """
-    Get all recipe lists
-    """
-    pass
-
-
-
+#SMOKE TESTED
 @app.post(
     '/recipe-lists', response_model=None, responses={'201': {'model': RecipeList}}
 )
-def post_recipe_lists(body: RecipeList) -> Union[None, RecipeList]:
+def post_recipe_lists(body: CreateRecipeListRequest) -> Union[None, RecipeList]:
     """
     Create a new recipe list
     """
-    pass
+    with engine.begin() as conn:
+        result = conn.execute(text(f"""INSERT INTO recipe_list (name, description)
+                                    VALUES (:name, :description)
+                                    RETURNING id, name, description 
+                                   """
+                                    ),{"name":body.name,"description":body.description})
+        
+        id, name, description= result.fetchone()
+        return RecipeList(id=id, name=name, description=description)
 
-@app.get('/recipe-lists/{id}', response_model=RecipeList)
+#SMOKE TESTED
+@app.get('/recipe-lists/{id}', response_model=None)
 def get_recipe_list(id: int) -> RecipeList:
     """
     Get a recipe list by id
     """
-    pass
+    with engine.begin() as conn:
+        result = conn.execute(text(f"""SELECT id, name, description FROM recipe_list WHERE id = :recipe_id"""),{"recipe_id": id})
+        id, name, description = result.fetchone()
+        return RecipeList(id=id, name=name, description= description)
 
 @app.post("/recipe-lists/{id}")
 def update_recipe_list(id: int, recipe_list : RecipeList) -> RecipeList:
@@ -93,63 +106,58 @@ def update_recipe_list(id: int, recipe_list : RecipeList) -> RecipeList:
 def delete_recipe_list(id: int) -> None:
     pass
 
-
+#TODO: fixme and implement search
 @app.get('/recipes', response_model=List[Recipe])
 def get_recipes() -> List[Recipe]:
     """
     Get all recipes
     """
-    pass
+    with engine.begin() as conn:
+        result = conn.execute(text(f"""SELECT id, name, email, phone FROM "user" """))
+        id, name, email, phone = result.fetchone()
+        return User(id=id, name=name, email=email, phone=phone)
+    
 
-
-@app.post('/recipes', response_model=None, responses={'201': {'model': PopulatedRecipe}})
-def post_recipes(body: PopulatedRecipe) -> Union[None, PopulatedRecipe]:
+#SMOKE TESTED
+#FIXME: increment created at in database
+@app.post('/recipes', response_model=None, responses={'201': {'model': CreateRecipeRequest}})
+def post_recipes(body: CreateRecipeRequest) -> Union[None, Recipe]:
     """
     Create a new recipe
-    """
-    tags = body.tags
-    
+    """    
     with engine.begin() as conn:
-        result = conn.execute(text(f"""INSERT INTO recipe (name, mins_prep, mins_cook, description, default_servings, author_id)
+        result = conn.execute(text(f"""INSERT INTO recipe (name, mins_prep, mins_cook, description, default_servings, author_id, procedure)
                                    VALUES (
                                    :name,
                                    :mins_prep,
                                    :mins_cook,
                                    :description,
                                    :default_servings,
-                                   :author_id)"""
+                                   :author_id,
+                                   :procedure)
+                                   RETURNING id, name, mins_prep, mins_cook, description, default_servings, author_id, procedure"""
                                    
             ), {"name":body.name,
-             "author_id":body.author['id'],
+             "author_id":body.author_id,
              "mins_prep":body.mins_prep,
              "mins_cook":body.mins_cook
              ,"description":body.description,
-             "default_servings":body.default_servings})
-        id,name,mins_prep,mins_cook,description,default_servings,author_id = result.fetchone()
-        recipe = PopulatedRecipe(id=id,name=name,mins_prep=mins_prep,mins_cook=mins_cook,description=description,default_servings=default_servings,author_id=author_id)
-
-        #FIXME: make this one query
-        for tag in tags:
-            result = conn.execute(text(f"""INSERT INTO tag_x_recipe
-                            VALUES (
-                            :tag_id,
-                            :recipe_id)"""
-                            ,
-            {"tag_id":tag.id,
-                "recipe_id":body.id}))
-            recipe.tags.append(tag)
+             "default_servings":body.default_servings,
+             "procedure":body.procedure})
+        id,name,mins_prep,mins_cook,description,default_servings,author_id,procedure = result.fetchone()
+        recipe = Recipe(id=id,name=name,mins_prep=mins_prep,mins_cook=mins_cook,description=description,default_servings=default_servings,author_id=author_id, procedure=procedure)
         return recipe
-        
-        
-    
 
-
+#SMOKE TESTED
 @app.get('/recipes/{id}', response_model=Recipe)
 def get_recipe(id: int) -> Recipe:
     """
     Get a recipe by id
     """
-    pass
+    with engine.begin() as conn:
+        result = conn.execute(text(f"""SELECT id, name, mins_prep, mins_cook, description, default_servings, author_id, procedure FROM recipe WHERE id = :id"""),{"id":id})
+        id, name, mins_prep,mins_cook,description,default_servings,author_id,procedure = result.fetchone()
+        return Recipe(id=id,name=name,mins_prep=mins_prep,mins_cook=mins_cook,description=description,default_servings=default_servings,author_id=author_id, procedure=procedure)
 
 @app.post("/recipes/{id}")
 def update_recipe(id: int, recipe : Recipe) -> Recipe:
@@ -158,6 +166,12 @@ def update_recipe(id: int, recipe : Recipe) -> Recipe:
 @app.delete("/recipes/{id}")
 def delete_recipe(id: int) -> None:
     pass
+
+@app.post('/recipes/{recipe_id}/recipe-lists/{recipe_list_id}', response_model=None)
+def add_recipe_to_recipe_list(recipe_id: int, recipe_list_id: int) -> None:
+    with engine.begin() as conn:
+        conn.execute(text(f"INSERT INTO recipe_x_recipe_list (recipe_id, recipe_list_id) VALUES (:recipe_id, :recipe_list_id)"),{"recipe_id":recipe_id,"recipe_list_id":recipe_list_id})
+        return "OK"
 
 @app.get('/reviews', response_model=List[Review])
 def get_reviews() -> List[Review]:
@@ -200,30 +214,34 @@ def get_users() -> List[User]:
     Get all users
     """
     with engine.begin() as conn:
-        result = conn.execute(text(f"SELECT id, name, email, phone FROM users ORDER BY id"))
+        result = conn.execute(text(f"SELECT id, name, email, phone FROM user ORDER BY id"))
         id, name, email, phone = result.fetchone()
         return User(id=id, name=name, email=email, phone=phone)
 
+#SMOKE TESTED
 @app.get('/users/{user_id}',response_model=User)
 def get_user(user_id: int) -> List[User]:
     """
-    Get all users
+    Get one user
     """
     with engine.begin() as conn:
-        result = conn.execute(text(f"""SELECT id, name, email, phone FROM "user" WHERE id = {user_id}"""))
+        result = conn.execute(text(f"""SELECT id, name, email, phone FROM "user" WHERE id = :user_id"""),{"user_id":user_id})
         id, name, email, phone = result.fetchone()
         return User(id=id, name=name, email=email, phone=phone)
 
+#SMOKE TESTED
 @app.post('/users', response_model=None, responses={'201': {'model': User}})
-def post_users(body: User) -> Union[None, User]:
+def post_users(body: CreateUserRequest) -> Union[None, User]:
     """
     Create a new user
     """
     with engine.begin() as conn:
         result = conn.execute(text(f"""INSERT INTO "user" (name, email, phone)
                                     VALUES (:name, :email, :phone)
-                                    RETURNING id, name, email, phone"""
+                                    RETURNING id, name, email, phone
+                                   """
                                     ),{"name":body.name,"phone":body.phone,"email":body.email})
+        
         id, name, email, phone = result.fetchone()
         return User(id=id, name=name, email=email, phone=phone)
 
