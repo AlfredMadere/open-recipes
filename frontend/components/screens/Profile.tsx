73db4a4b7cea1c29ac38730d57092305a07413af
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2023-10-30 23:48:33 -0700

    creating template files

---
+++ frontend/components/screens/Profile.tsx
@@ -0,0 +1,27 @@
+import { Button, H1, Paragraph, Text, View } from "tamagui";
+import { StyleSheet } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <H1>Welcome 307 Team!</H1>
+      <Paragraph>
+        This we will be using tamagui I made A COOL CHANFEwhich makes front end
+        YOOOO development hella easy and good looking. Use the tamagui
+        components!
+      </Paragraph>
+      <Button onPress={() => navigation.navigate("Details")}>
+        <Text>Go to Details</Text>
+      </Button>
+    </View>
+  );
+}
