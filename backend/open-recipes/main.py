67ce34191d6805c43ebfe624a8ef313e688bd23d
Mihir <mihir1003@gmail.com>
2023-10-30 22:49:34 -0700

    fix

--- backend/open-recipes/main.py
+++ backend/open-recipes/main.py
@@ -277,7 +277,7 @@ import uvicorn
 
 if __name__ == "__main__":
     config = uvicorn.Config(
-        "open-recipes.main:app", port=3000, log_level="info", reload=True, env_file=".env"
+        ".:app", port=3000, log_level="info", reload=True, env_file=".env"
     )
     server = uvicorn.Server(config)
     server.run()
