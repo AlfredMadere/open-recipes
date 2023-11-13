6ae30200200d84739b876160903a13f6238dca25
Michael Murray <michael.murray.iv@gmail.com>
2023-11-13 12:49:16 -0800

    implemented react-hook-form for creating a recipe. Still need to figure out how to retrieve the data from the form

--- frontend/app/(tabs)/create-recipe.tsx
+++ frontend/app/(tabs)/create-recipe.tsx
@@ -1,41 +1,335 @@
 import { useRouter } from "expo-router";
-import React, { useState } from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
-import Table from "../../components/table";
-import Form from "../../components/form";
+import React, { useEffect } from "react";
 import { Link } from "expo-router";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
+import { Text, View, StyleSheet, TextInput, Button, Alert } from "react-native";
+import { ScrollView } from "tamagui";
 
 export default function Page() {
-  const router = useRouter();
-  const [recipes, setRecipes] = useState([]);
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+    getValues,
+  } = useForm({
+    defaultValues: {
+      name: "",
+      description: "",
+      servings: "",
+      mins_prep: "",
+      mins_cook: "",
+      fieldArray: [],
+      fieldArray2: [],
+
+    },
+    // defaultValues: {
+    //   value1: [{ email: "Bill", password: "Luo" }],
+    // },
+  });
+
+  const {
+    fields: fields1,
+    append: append1,
+    remove: remove1,
+  } = useFieldArray({
+    control, // control props comes from useForm (optional: if you are using FormContext)
+    name: "fieldArray", // unique name for your Field Array
+  });
+  const {
+    fields: fields2,
+    append: append2,
+    remove: remove2,
+  } = useFieldArray({
+    control, // control props comes from useForm (optional: if you are using FormContext)
+    name: "fieldArray2", // unique name for your Field Array
+  });
+
+  const onSubmit = (data: any) => {
+    
+    console.log('Form Data: ', data);
+
+    const items=getValues('fieldArray2');
+    console.log(items);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Form />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title="Cancel" color="red" />
-        <Button title="Create" color="green" />
+      <ScrollView style={styles.scrollViewContent}>
+        <Text style={styles.label}>Title</Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value} }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+          name="name"
+          rules={{ required: true }}
+        />
+
+        <Text style={styles.label}>Description</Text>
+
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+          name="description"
+          rules={{ required: true }}
+        />
+        <Text style={styles.label}>Servings</Text>
+        <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+          name="servings"
+          rules={{ required: true }}
+        />
+
+        <Text style={styles.label}>Time</Text>
+        <View style={styles.ingredient}>
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder={`Prep Time`}
+                placeholderTextColor={"gray"}
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+                value={value}
+              />
+            )}
+            name="mins_prep"
+            rules={{ required: true }}
+          />
+          <Controller
+            control={control}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                placeholder={`Cook Time`}
+                placeholderTextColor={"gray"}
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={(value) => onChange(value)}
+                value={value}
+              />
+            )}
+            name="mins_cook"
+            rules={{ required: true }}
+          />
+        </View>
+
+        <Text style={styles.label}>Tags</Text>
+        {fields1.map((field, index) => (
+          <View key={field.id} style={styles.container}>
+            <View style={styles.ingredient}>
+              <Controller
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder={`Key`}
+                    placeholderTextColor={"gray"}
+                    onChangeText={field.onChange}
+                    value={field.value}
+                  />
+                )}
+                name={`key[${index}].value`}
+                defaultValue=""
+              />
+              <Controller
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder={`Value`}
+                    placeholderTextColor={"gray"}
+                    onChangeText={field.onChange}
+                    value={field.value}
+                  />
+                )}
+                name={`value[${index}].value`}
+                defaultValue=""
+              />
+
+              <View style={styles.smallButton}>
+                <Button
+                  onPress={() => {
+                    setValue(`key[${index}].value`, "");
+                    setValue(`value[${index}].value`, "");
+                    remove1(index);
+                  }}
+                  title="Remove"
+                  color="white"
+                />
+              </View>
+            </View>
+          </View>
+        ))}
+        <View style={styles.smallButton}>
+          <Button
+            title="Add Tag"
+            color="white"
+            onPress={() => {
+              append1({ value: "" });
+            }}
+          />
+        </View>
+
+        <Text style={styles.label}>Ingredients</Text>
+        {fields2.map((field, index) => (
+          <View key={field.id} style={styles.container}>
+            <View style={styles.ingredient}>
+              <Controller
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder={`Quantity`}
+                    placeholderTextColor={"gray"}
+                    onChangeText={field.onChange}
+                    value={field.value}
+                  />
+                  
+                )}
+                name={`quantity[${index}].value`}
+                defaultValue=""
+              />
+              <Controller
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder={`Units`}
+                    placeholderTextColor={"gray"}
+                    onChangeText={field.onChange}
+                    value={field.value}
+                  />
+                )}
+                name={`units[${index}].value`}
+                defaultValue=""
+              />
+              <Controller
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    style={styles.input}
+                    placeholder={`Ingredient`}
+                    placeholderTextColor={"gray"}
+                    onChangeText={field.onChange}
+                    value={field.value}
+                  />
+                )}
+                name={`ingredients[${index}].value`}
+                defaultValue=""
+              />
+
+              <View style={styles.smallButton}>
+                <Button
+                  onPress={() => {
+                    setValue(`ingredients[${index}].value`, "");
+                    setValue(`units[${index}].value`, "");
+                    setValue(`quantity[${index}].value`, "");
+                    remove2(index);
+                  }}
+                  title="Remove"
+                  color="white"
+                />
+              </View>
+            </View>
+          </View>
+        ))}
+
+        <View style={styles.smallButton}>
+          <Button
+            title="Add Ingredient"
+            color="white"
+            onPress={() => {
+              append2({ value: "" });
+            }}
+          />
+        </View>
+      </ScrollView>
+      <View style={styles.footer}>
+        <Button
+          title="Cancel"
+          color="red"
+          onPress={() => {
+            reset({
+              name: "",
+              description: "",
+            });
+          }}
+        />
+        <Button title="Create" color="green" onPress={handleSubmit(onSubmit)} />
       </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
+  scrollViewContent: {
+    flexGrow: 1,
+  },
+  label: {
+    color: "white",
+    margin: 20,
+    marginLeft: 0,
+  },
+  footer: {
+    marginTop: 40,
+    color: "white",
+    height: 40,
+    backgroundColor: "gray",
+    borderRadius: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  ingredient: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: 'space-between',
+    margin: 0,
+    padding: 0,
+  },
+  smallButton: {
+    marginTop: 0,
+    color: "white",
+    height: 40,
+    backgroundColor: "gray",
+    borderRadius: 4,
+    justifyContent: "flex-end",
+    flex: 0,
+    flexDirection: "row",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
+    paddingTop: 20,
+    padding: 8,
+    backgroundColor: "gray",
   },
-  content: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 16,
+  input: {
+    backgroundColor: "white",
+    height: 40,
+    borderRadius: 4,
+    padding: 10,
   },
 });
