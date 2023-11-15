ad3920776e5e4acea15cd681e096de02c683fb27
Kassandra Winter <kassandrawinter@Kassandras-MacBook-Pro-2.local>
2023-11-15 14:37:27 -0800

    form broken on submit

--- frontend/app/(tabs)/lists/index.tsx
+++ frontend/app/(tabs)/lists/index.tsx
@@ -1,179 +1,269 @@
 import { useRouter } from "expo-router";
-import { Button, View, Stack, Card, XStack } from "tamagui";
-import {Text, FlatList, Modal, Alert, TextInput, SafeAreaView} from 'react-native';
-import React, {useState} from "react";
+import { View, Stack, Card, XStack } from "tamagui";
+import { useForm, Controller } from "react-hook-form";
+import {
+  Text,
+  FlatList,
+  Modal,
+  Alert,
+  TextInput,
+  Button,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 
 
 export default function One() {
-  const router = useRouter();
   const [modalVisible, setModalVisible] = useState(false);
-  const [name, onChangeNameText] = React.useState('');
-  const [description, onChangeDescriptionText] = React.useState('');
+  const [lists, setLists] = useState([]);
+
+  const {
+    reset,
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  })
+  const name = watch ("name"); //see if it's populated in the console
+  const description = watch ("description");
+  console.log("name: ", name)
+  console.log("description: ", description)
+  // try commenting out sections at a time to see if it is specific sections affecting it
+ 
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch('https://open-recipes.onrender.com/recipe-lists', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to add data');
+      }
+
+      fetchDataFromBackend();
+    } catch (error) {
+      console.error('Error adding data:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataFromBackend();
+  }, []);
+
+  const fetchDataFromBackend = async () => {
+    try {
+      const response = await fetch('https://open-recipes.onrender.com/recipe-lists');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+
+      const data = await response.json();
+      setLists(data);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  };
 
 
-  let lists = [
-    {
-      name: "Breakfast",
-      description: "Some description here...",
-    },{
-      name: "Lunch",
-      description: "Some description here...",
-    },
-    {
-      name: "Dinner",
-      description: "Some description here...",
-    },
-    {
-      name: "Snacks",
-      description: "Some description here...",
-    },
-    {
-      name: "Desserts",
-      description: "Some description here...",
 
-    },
-  ];
-
-  
   return (
-    <View style={{width: '100%', flex: 1}}>
-    
-      <View style={{flex: 1, marginVertical: 20}}>
-
-          <View style={{ flex: 1 }}>
-           <ListComponent data={lists} />
-          </View>
+    <View style={{ width: "100%", flex: 1 }}>
+      <View style={{ flex: 1, marginVertical: 20 }}>
+        <View style={{ flex: 1 }}>
+          <ListComponent data={lists} />
+        </View>
       </View>
-      <View style={{alignSelf: 'flex-end'}}>
-          <Stack scale={1.2} marginTop={15}>
-        
-        <Modal
-          animationType="none"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-            setModalVisible(!modalVisible);
-          }}>
-           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <View style={{margin: 20,
-                backgroundColor: 'white',
-                height: 350,
-                width: 335,
-                borderRadius: 20,
-                padding: 35,
-                alignItems: 'center',
-                shadowColor: '#000',
-                shadowOffset: {
-                  width: 0,
-                  height: 2,
-                },
-                shadowOpacity: 0.25,
-                shadowRadius: 4,
-                elevation: 5,}}>
-          <View>
-          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ textDecorationLine: 'underline' }}>Create New List</Text>
-          </View>
-            <Button 
-              style = {{color: 'red',  backgroundColor: 'white', position: 'absolute', top:-20, right:-50, width:50, height:50}} 
-              onPress={() => setModalVisible(!modalVisible)}
-              >X</Button>
+      <View style={{ alignSelf: "flex-end" }}>
+        <Stack scale={1.2} marginTop={15}>
+          <Modal
+            animationType="none"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  margin: 20,
+                  backgroundColor: "white",
+                  height: 350,
+                  width: 335,
+                  borderRadius: 20,
+                  padding: 35,
+                  alignItems: "center",
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 2,
+                  },
+                  shadowOpacity: 0.25,
+                  shadowRadius: 4,
+                  elevation: 5,
+                }}
+              >
+                <View>
+                  <View
+                    style={{ justifyContent: "center", alignItems: "center" }}
+                  >
+                    <View>
+                    <Text style={{ textDecorationLine: "underline" }}>
+                      Create New List
+                    </Text>
+                  </View>
+                
+                  <View style={{ height: 300, paddingTop: 25 }}>
+                   
+                  <Text>Title:</Text>
+                  <Controller
+                    control={control}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                     style={{
+                      height: 40,
+                      width: 200,
+                      margin: 12,
+                      borderWidth: 1,
+                      padding: 10,
+                     }}
+                      onBlur={onBlur}
+                      onChangeText={(value) => onChange(value)}
+                      value={value}
+                       />
+                     )}
+                      name="name"
+                      rules={{ required: true }}
+                     />
+                     {errors.name && <Text>This is required.</Text>}
+                    
+                    <Text>Description</Text>
+                    <Controller
+                      control={control}
+                      render={({ field: { onChange, onBlur, value } }) => (
+                      <TextInput
+                      style={{
+                        height: 40,
+                        width: 200,
+                        margin: 12,
+                        borderWidth: 1,
+                        padding: 10,
+                      }}
+                        onBlur={onBlur}
+                        onChangeText={(value) => onChange(value)}
+                        value={value}/>
+                      )}
+                     name="description"
+                     rules={{ maxLength: 100, }}
+                     />
+                      {errors.description && <Text>This description has exceeded the word count.</Text>}
+                    
+                    <View style={{ padding: 20 }}>
+                    <Button onPress={() => {
+                      setModalVisible(!modalVisible)
+                      handleSubmit(onSubmit)
+                      }}
+                    title="Create"
+                    color="green"
+                    />
 
-          <View style={{ height: 300, paddingTop: 25 }}>
-            <Text>Name:</Text>
-            <SafeAreaView>
-              <TextInput
-                  onChangeText={onChangeNameText}
-                  value={name}
-                  style = {{ height: 40, width: 200,
-                    margin: 12,
-                    borderWidth: 1,
-                    padding: 10,}}
-                />
-            </SafeAreaView>
-            <Text> </Text> 
-            <Text>Description:</Text>
-            <SafeAreaView>
-              <TextInput
-                  onChangeText={onChangeDescriptionText}
-                  value={description}
-                  style = {{ height: 40,
-                    margin: 12,
-                    borderWidth: 1,
-                    padding: 10,}}
-                />
-            </SafeAreaView>
-            <View style={{padding: 20}}>
-            <Button
-              onPress={() => setModalVisible(!modalVisible)}>Save</Button>
+                    <Button
+                      onPress={() => {
+                      setModalVisible(!modalVisible)
+                      reset({
+                        name: "",
+                        description: "",
+                      });
+                      }}
+                    title="Cancel"
+                    color="red"
+                    />
+
+                    </View>
+                    </View>
+                  </View>
+                </View>
+              </View>
             </View>
-          </View>
-          </View>
-        </View>
-        </View>
-      </Modal>
-      
+          </Modal>
 
-          <Button onPress={() => setModalVisible(true)}>New List</Button>
-          </Stack>
-        </View> 
+          <Button
+                   onPress={() => {
+                    setModalVisible(true)
+                    reset({
+                      name: "",
+                      description: "",
+                    });
+                    }}
+                  title="New List"
+                  color="blue"
+                  />
+        </Stack>
+      </View>
     </View>
   );
 }
 
-
-
-const ListComponent = ({ data }) => {
+const ListComponent = ({ data }: any) => {
   return (
     <FlatList
       data={data}
       numColumns={1}
       renderItem={({ item }) => (
-        <View style={{ flex: 1, marginLeft: 45,
-          marginRight: 45,
-          marginTop: 20,
-          width: "100%",
-          alignItems: "stretch",
-          justifyContent: "center",
-        }}
-        space>
-       <ListCard
-            name={item.name}
-            description={item.description}
-          />
-         
+        <View
+          style={{
+            flex: 1,
+            marginLeft: 45,
+            marginRight: 45,
+            marginTop: 20,
+            width: "100%",
+            alignItems: "stretch",
+            justifyContent: "center",
+          }}
+          space
+        >
+          <ListCard name={item.name} description={item.description} />
         </View>
       )}
-
     />
   );
 };
-
-
 
 export function ListCard(props: { name: string; description: string }) {
   const { name, description } = props;
   const router = useRouter();
 
   return (
-    <Card
-      elevate
-      size="4"
-      width={305}
-      height={120}
-      bordered
-    >
+    <Card elevate size="4" width={305} height={120} bordered>
       <Card.Header padded>
-      <Text>{name}</Text>
+        <Text>{name}</Text>
       </Card.Header>
       <Card.Footer padded>
         <XStack flex={1} />
         <Text>{description}</Text>
-          <Button alignSelf='center' borderRadius="$2" onPress={() => {router.push('/lists/[id]')}}>View</Button>
+        <Button
+            onPress={() => {
+              router.push("/lists/[id]");
+            }}
+            title="View"
+            color="blue"
+        />
       </Card.Footer>
     </Card>
   );
 }
-
-
