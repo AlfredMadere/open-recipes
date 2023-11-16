e65b11b5750b137e6a74fb41916b36de8fbec19d
PauMinguet <pablitumm@gmail.com>
2023-11-16 13:52:37 -0800

    tags and ingrs

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -102,93 +102,23 @@ export function RecipeCard(props: RecipeCardProps) {
   const router = useRouter();
 
   return (
-    <Card size="$4" width={"45%"} height={60} bordered {...props}>
-      <Button
-          height={60}
-          color={'#00000'}
+    <Card elevate size="$4" width={"100%"} height={70} bordered {...props}>
+      <Card.Header padded>
+        <Text>{recipe.name}</Text>
+        <Paragraph theme="alt2">{recipe.description}</Paragraph>
+      </Card.Header>
+      <Card.Footer padded>
+        <XStack flex={1} />
+        <Button
+          borderRadius="$10"
           onPress={() => {
             router.push(`/recipes/${recipe.id}`);
           }}
         >
-          {recipe.name}
+          View
         </Button>
+      </Card.Footer>
     </Card>
   );
 }
 
-//   return (
-//     <View style={{width: '100%', flex: 1}}>
-//       {query.error && <Text>{JSON.stringify(query.error)}</Text>}
-//       {query.isFetching && <Spinner size="large" color="$orange10" />}
-//       <View style={{flex: 1, marginVertical: 20}}>
-//         <View style={{alignSelf: 'center'}}>
-//           <Circle size={100} backgroundColor="$color" elevation="$4" />
-//           <Stack scale={1.2} marginTop={15}>
-//             <Text>{username}</Text>
-//           </Stack>
-//         </View>
-//         <View style={{marginLeft:10, marginTop:20}}>
-//             <Text>Pinned Recipes:</Text>
-//         </View>
-
-//           <View style={{ flex: 1 }}>
-//            <GridComponent query={query} />
-//           </View>
-
-//       </View>
-//     </View>
-//   );
-// }
-
-// const GridComponent = ({ query }) => {
-//   return (
-//     <FlatList
-//       data={query}
-//       numColumns={3}
-//       keyExtractor={(item, index) => index.toString()}
-//       renderItem={({ item }) => (
-
-//         <View style={{ flex: 1, marginLeft: 45,
-//           marginRight: 45,
-//           marginTop: 20,
-//           flexDirection: "column",
-//           width: "100%",
-//           alignItems: "center",
-//           justifyContent: "center",
-//         }}
-//         paddingHorizontal="$4"
-//         space>
-
-//           {query.data?.recipe.map((recipe) => {
-//             return <RecipeCard key={recipe.id} recipe={recipe} />;
-//           })}
-
-//         </View>
-//       )}
-//     />
-//   );
-// };
-
-// export function RecipeCard(props: RecipeCardProps) {
-//   const recipe = props.recipe
-//   const router = useRouter();
-
-//   return (
-//     <Card
-//       elevate
-//       size="4"
-//       width={120}
-//       height={120}
-//       bordered
-//       {...props}
-//     >
-//       <Card.Header padded>
-//         <Text>{recipe.name}</Text>
-//       </Card.Header>
-//       <Card.Footer padded>
-//         <XStack flex={1} />
-//         <Button alignSelf='center' borderRadius="$2" onPress={() => {router.push(`/recipes/${recipe.id}`)}}>View</Button>
-//       </Card.Footer>
-//     </Card>
-//   );
-// }
--- frontend/app/recipes/[id].tsx
+++ frontend/app/recipes/[id].tsx
@@ -16,19 +16,69 @@ const Register = () => {
     console.log("response.data", response.data);
     return response.data;
   }
+
   const query = useQuery({ queryKey: ["recipe", id], queryFn: getRecipe });
+  
+  const { isLoading, data } = query; // Assuming 'query' is a hook or context providing recipe data
+
+  if (isLoading) {
+    return <Spinner size="large" padding={50} />;
+  }
+
+  const ingredients = data?.ingredients || []; // Assuming ingredients are stored in an array
+  const tags = data?.tags || []; // Assuming tags are stored in an array
+
+
+  type RecipeCardProps = {
+    recipe: {
+      name: string;
+      description: string;
+      id: number;
+      mins_prep: number;
+      mins_cook: number;
+      category_id: number;
+      author_id: number;
+      created_at: string;
+      procedure: string;
+      default_servings: number;
+    };
+  };
+
+
+
+  
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.recipeDetails}>
-        <Text style={styles.title}>{query.data?.name}</Text>
-        <Text style={styles.description}>{query.data?.description}</Text>
+        <Text style={styles.title}>{data?.name}</Text>
+        <Text style={styles.infoItem}>Author id: {data?.author_id}</Text>
+        <Text style={styles.description}>{data?.description}</Text>
+
         <View style={styles.info}>
-          <Text style={styles.infoItem}>Prep Time: {query.data?.mins_prep} mins</Text>
-          <Text style={styles.infoItem}>Cook Time: {query.data?.mins_cook} mins</Text>
-          <Text style={styles.infoItem}>Servings: {query.data?.default_servings}</Text>
+          <Text style={styles.infoItem}>Prep Time: {data?.mins_prep} mins</Text>
+          <Text style={styles.infoItem}>Cook Time: {data?.mins_cook} mins</Text>
+          <Text style={styles.infoItem}>Servings: {data?.default_servings}</Text>
         </View>
-        <Text style={styles.infoItem}>Procedure:</Text>
-        <Text style={styles.procedure}>{query.data?.procedure}</Text>
+
+        <Text style={styles.description}>Procedure: {data?.procedure}</Text>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Ingredients:</Text>
+          {ingredients.map((ingredient, index) => (
+            <Text key={index} style={styles.infoItem}>
+              - {ingredient}
+            </Text>
+          ))}
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Tags:</Text>
+          {tags.map((tag, index) => (
+            <Text key={index} style={styles.infoItem}>
+              - {tag}
+            </Text>
+          ))}
+        </View>
       </View>
     </ScrollView>
   );
@@ -38,8 +88,8 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     backgroundColor: '#f0f0f0',
-    paddingHorizontal: 10,
-    paddingTop: 20,
+    paddingHorizontal: 20,
+    paddingTop: 40,
   },
   recipeDetails: {
     backgroundColor: '#fff',
@@ -49,41 +99,39 @@ const styles = StyleSheet.create({
     elevation: 3,
   },
   title: {
-    justifyContent: 'center',
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 10,
   },
   description: {
     fontSize: 18,
     marginBottom: 10,
+    marginTop: 10,
+
   },
   info: {
     justifyContent: 'space-between',
-    marginBottom: 15,
+    marginBottom: 5,
   },
   infoItem: {
     fontSize: 16,
     fontWeight: 'bold',
+    marginTop: 5,
   },
-  procedure: {
+  section: {
+    marginTop: 15,
+  },
+  sectionTitle: {
     fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  procedureStep: {
+    fontSize: 16,
+    marginBottom: 8,
   },
 });
 
-type RecipeCardProps = {
-  recipe: {
-    name: string;
-    description: string;
-    id: number;
-    mins_prep: number;
-    mins_cook: number;
-    category_id: number;
-    author_id: number;
-    created_at: string;
-    procedure: string;
-    default_servings: number;
-  };
-};
+
 
 export default Register;
